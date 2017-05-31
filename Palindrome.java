48398d4d64462062a28ca3ac6dcb08d674d1174e
salman mujtaba <salmanmujtaba1793@gmail.com>
2017-05-31 00:38:21 -0400

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -1,14 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package coll;
-
-/**
- *
- * @author KashishSyeda
- */
 public class Palindrome {
     
     public static void main(String[] args) {
